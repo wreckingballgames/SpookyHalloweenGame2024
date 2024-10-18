c899da2eef18c95cc429392c076e0d48fe3ea796
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-18 13:52:52 -0500

    Add `PlayerStatistics` Resource and assign a default one to `PlayerCharacter`

--- neighbors/scripts/player/player_character.gd
+++ neighbors/scripts/player/player_character.gd
@@ -1,5 +1,11 @@
 class_name PlayerCharacter
 extends CharacterBody2D
 
+@export var stats: PlayerStatistics
+
 # Should only be modified by PlayerController
 var id: int
+
+
+func _process(delta: float) -> void:
+	move_and_slide()
