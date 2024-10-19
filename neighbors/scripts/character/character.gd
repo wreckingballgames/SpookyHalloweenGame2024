04316e2bd2884b8dfa28b482e54ca09651743ca3
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-19 15:43:00 -0500

    Document `Character` and change its `stats` property to type `CharacterStatistics`

--- neighbors/scripts/character/character.gd
+++ neighbors/scripts/character/character.gd
@@ -1,14 +1,21 @@
 class_name Character
 extends CharacterBody2D
+## The base class for all characters in the game. This should be extended before creating a new Character scene.
 
-@export var stats: PlayerStatistics
+## The character's unique attributes.
+@export var stats: CharacterStatistics
+## The weapon the character begins play equipped with.
 @export var starting_weapon: PackedScene
 
-# Should only be modified by the character's associated controller!
+## The character's unique identifier. This should only be modified by the character's CharacterController.
 var id: int
+## The direction the character is facing.
 var direction: Vector2 = Vector2.ZERO
+## The position the character is "focused" on. Most characters look at this position and most enemies move toward it.
 var target_position: Vector2 = Vector2.ZERO
+## The location of the character's weapon.
 var weapon_origin: Vector2
+## A reference to the player's currently equipped weapon.
 var equipped_weapon: Weapon
 
 
@@ -22,6 +29,7 @@ func _process(_delta: float) -> void:
 	look_at(target_position)
 
 
+## Returns the location where the character's weapon should be placed or the character's position if no valid WeaponOrigin is found.
 func get_weapon_origin() -> Vector2:
 	# TODO: determine if characters can ever have more than one weapon origin
 	#   assume they won't for now. This code is easy to refactor if they do
@@ -32,6 +40,7 @@ func get_weapon_origin() -> Vector2:
 	return global_position
 
 
+## Unequip the character's previous weapon, if any, and equip a new one.
 func equip_weapon(weapon: PackedScene) -> void:
 	# Unequip previous weapon
 	if equipped_weapon:
