72459a4d0ea4f886966210aac93e840f459a1df3
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-19 17:48:37 -0500

    Document `CharacterController`

--- neighbors/scripts/character/character_controller.gd
+++ neighbors/scripts/character/character_controller.gd
@@ -1,8 +1,11 @@
 class_name CharacterController
 extends Node
+## The base class for all character controllers. Extend before using. A controller is an authoritative source of input handling for Characters.
 
+## Reference to the controller's associated character.
 @export var character: Character
 
+## Unique identifier, generated in collaboration with the multiplayer object.
 var id: int:
 	set(value):
 		id = value
