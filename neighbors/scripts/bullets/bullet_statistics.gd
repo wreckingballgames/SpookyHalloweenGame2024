30c36b1d5ba5d7d569b5ff85f45fc6764cc95def
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-19 15:30:19 -0500

    Document `BulletStatistics`

--- neighbors/scripts/bullets/bullet_statistics.gd
+++ neighbors/scripts/bullets/bullet_statistics.gd
@@ -1,10 +1,15 @@
 class_name BulletStatistics
 extends Resource
+## This Resource determines all of a bullet's unique attributes.
 
+## The bullet's base damage value.
 @export var attack_power: int
+## The bullet's in-game texture.
 @export var sprite: Texture
+## The bullet's UI texture.
 @export var icon: Texture
-# lifespan is the amount of time in seconds before bullet expires
+## The time in seconds before the bullet expires after spawn.
 @export var lifespan: float
 # TODO: Imagine and add all kinds of bullet properties like the one below
+## Whether or not the bullet continues moving after colliding with a physics body.
 @export var is_piercing: bool
