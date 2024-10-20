aeea1930a49c49d97c0580afc3fcb75cbdf7294a
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-19 21:06:20 -0500

    Add a `Constants` autoload, implement `Item`s, add some items, and create a script for `ItemEffect`s
    `ItemEffect`s won't be implemented for a while. `Item`s are simple `Resource` files that hold data and don't need to track how they're used by things like `Pickup`s and `ItemEffect`s

---
+++ neighbors/scripts/globals/constants.gd
@@ -0,0 +1,16 @@
+extends Node
+## An autoload script to hold useful information for use across the entire game.
+
+## All of the types of items usable by Characters in the game.
+enum item_types {
+	MEDKIT,
+	KEY,
+	DECOY,
+	SHAPED_CHARGE,
+}
+
+enum item_rarities {
+	COMMON,
+	UNCOMMON,
+	RARE,
+}
