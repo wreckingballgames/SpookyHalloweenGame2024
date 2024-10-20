e1ab2e24ccb96de62600e81c34e145faee4f4fa8
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-19 20:07:08 -0500

    Document `WeaponStatistics`

--- neighbors/scripts/weapons/weapon_statistics.gd
+++ neighbors/scripts/weapons/weapon_statistics.gd
@@ -1,10 +1,16 @@
 class_name WeaponStatistics
 extends Resource
+## The Resource which determines all of a Weapon's unique attributes.
 
+## Human-readable name.
 @export var name: String = "A weapon"
+## In-game texture.
 @export var sprite: Texture
+## UI texture.
 @export var icon: Texture
+## The template used to spawn bullets. Only put bullet scenes here.
 @export var bullet_template: PackedScene
-# ammo_capacity = 0 means infinite ammo (melee weapons have 0)
+## The maximum ammo Character's can hold for this Weapon. Set to 0 for melee weapons or weapons with infinite ammo.
 @export var ammo_capacity: int = 0
+## The rate at which Weapons can spawn bullets in actions per second.
 @export var attack_rate: int = 1
