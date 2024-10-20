a9ca4d5afe9b4b84b85c2a55f758a2125ff6a559
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-19 20:03:46 -0500

    Document `Weapon` and make its `Sprite2D` child completely dynamic and programmatic.

--- neighbors/scripts/weapons/weapon.gd
+++ neighbors/scripts/weapons/weapon.gd
@@ -1,20 +1,25 @@
 class_name Weapon
 extends Node2D
+## The object Characters use to damage other Characters (or pseudo-Characters) with bullets.
 
+## The Weapon's unique attributes.
 @export var stats: WeaponStatistics
 
+## Reference to the Character which owns the Weapon.
 var character: Character
+## The Weapon's current remaining ammo. Melee weapons or weapons with infinite ammo always have an ammo_count of 0.
 var ammo_count: int
-
-# This script is used on more than one object
-# Ensure these objects exist with default, unique names
-@onready var sprite_2d: Sprite2D = %Sprite2D
+## The Weapon's in-game sprite.
+var sprite_2d: Sprite2D
 
 
 func _ready() -> void:
 	EventBus.attack_performed.connect(_on_attack_performed)
 
+	sprite_2d = Sprite2D.new()
 	sprite_2d.texture = stats.sprite
+	add_child(sprite_2d, true)
+	
 	ammo_count = stats.ammo_capacity
 
 
