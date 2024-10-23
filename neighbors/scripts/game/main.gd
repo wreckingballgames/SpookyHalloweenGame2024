777cee596afb7e3725694975d1365c43832b51f5
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-23 01:21:11 -0500

    Begin planning .xlf parsing journey in Main class

--- neighbors/scripts/game/main.gd
+++ neighbors/scripts/game/main.gd
@@ -2,3 +2,8 @@ class_name Main
 extends Node2D
 ## The main scene which encompasses all the game's activity, including levels,
 ## character, etc.
+
+
+# TODO: Write a parser to take user's language choice and a directory of .xlf
+# files to make localization string tables in the form of .tres files
+# - List high level steps to complete this task here
