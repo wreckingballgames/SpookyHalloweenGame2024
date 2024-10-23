0cf85da4bd61f33f7c2ca0f95f6d700408f0f025
Raymond McDonald <raymond@raymondmcdonald.net>
2024-10-23 18:16:02 -0500

    Modify default.xlf a bit and start work on XLIFF to Resource parser
    I may have hit a wall. To create custom Resources with programmatically set exported vars, you have to build a GDScript class for each kind.
    
    I would have to build a string for the .gd file for each <file>, create the file, then create Resources from that script and parse the <file>s again (or store all of the info in an array, actually) to set the values.
    
    I think this is all doable in GDScript, so I'll figure it out. This probably would have been way easier in C#, but hey, it's a good learning experience.

--- neighbors/scripts/game/main.gd
+++ neighbors/scripts/game/main.gd
@@ -2,8 +2,3 @@ class_name Main
 extends Node2D
 ## The main scene which encompasses all the game's activity, including levels,
 ## character, etc.
-
-
-# TODO: Write a parser to take user's language choice and a directory of .xlf
-# files to make localization string tables in the form of .tres files
-# - List high level steps to complete this task here
---
+++ neighbors/scripts/globals/localization_string_builder.gd
@@ -0,0 +1,66 @@
+@tool
+extends Node
+
+
+# TODO: Write a parser to take user's language choice and a directory of .xlf
+# files to make localization string tables in the form of .tres files
+# To store things in res:// instead of user://, you must do it in the editor.
+# I think it's okay to ship the game with all of the languages included instead
+# of building them when the user picks a language. So make an editor script to
+# do this.
+# - List high level steps to complete this task here
+
+
+# TODO: move this tool code to a button in the editor instead of doing on ready
+func _ready() -> void:
+	if Engine.is_editor_hint():
+		# Prefix for absolute file paths
+		var path_prefix: String
+		# Store all file names (which matches end of file path if you add .xlf)
+		var xliff_files: Array[String]
+		var parser := XMLParser.new()
+		# Get the paths to all the .xlf files first
+		parser.open("res://assets/xml/xliff_paths.xml")
+		# All nodes in this file should have only one attribute
+		while parser.read() != ERR_FILE_EOF:
+			if parser.get_node_type() == XMLParser.NODE_ELEMENT:
+				if parser.get_node_name() == "xliff_paths":
+					path_prefix = parser.get_attribute_value(0)
+				else:
+					xliff_files.append(parser.get_attribute_value(0))
+		# Use another path prefix for saving
+		var save_path_prefix = "res://resources/localization/strings/"
+		# Make directories for each culture
+		for file: String in xliff_files:
+			DirAccess.make_dir_absolute(path_prefix + file)
+		# TODO
+		# Dynamically build a .gd file that extends Resource and includes all of
+		# the exported fields my .tres files need, using the information from
+		# the .xlf files. Then create the Resources and set their values
+		#
+		# alternately TODO just do this outside of Godot like a smart person
+
+		# Parse each .xlf file and build a Resource
+		for file: String in xliff_files:
+			parser.open(path_prefix + file + ".xlf")
+			while parser.read() != ERR_FILE_EOF:
+				var node_name: String = parser.get_node_name()
+				# If open <file>
+				if parser.get_node_type() == XMLParser.NODE_ELEMENT and node_name == "file":
+					# Make a new resource corresponding to <file>, named after
+					# its id attribute
+					var new_resource := Resource.new()
+					var save_result := ResourceSaver.save(new_resource, save_path_prefix + file + "/" + file + ".tres")
+					print(save_result)
+		# For each .xlf file, parse the file and save a Resource for each
+		#  <file> to res://resources/localization/strings/<culture>
+		# Build the default culture folder first (en)
+		# For every other .xlf file, parse trgLang attribute from <xliff> root
+		#  to get name for culture folder
+		# Each Resource file should be named after the id attribute in its
+		#  corresponding <file>
+		# Each Resource should have a section with a #region labeling by
+		#  the id attribute of each <unit>
+		# Then each <segment> should become a field named after the id attribute
+		# The field's value will be the content of the <target> tags
+		#  (or <source> for the default culture)
